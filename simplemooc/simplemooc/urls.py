4ad644f587dd664404e5644f54f5b58d618bbf19
tiagomeurer@gmail.com <beat0804>
2018-01-26 00:47:51 -0200

    criação do banco de dados e configurações das views e urls

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home
 
 urlpatterns = [
-    path('', 'core.views.home', name='home')
+    path('admin/', admin.site.urls),
 ]
